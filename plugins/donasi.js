8eecdf7eceb01ff2d2fa07b7e817c65641434e93
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2021-11-08 05:09:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ uang akan dipakai oprasi bot
+├ gopay 085155333010
 └────
 `.trim(), '© stikerin', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
