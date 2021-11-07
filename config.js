7b7dba312f311c580a2bea02767780da842352ea
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2021-11-08 06:09:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ global.author = 'bot whatsapp'
 
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.fla = 'https://telegra.ph/file/a086e6db34481dee8258e.jpg'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
