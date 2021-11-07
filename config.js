2a40518f5fd576177209b7c540a550c628c2bf05
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2021-11-08 05:15:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,9 +24,9 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': '9c8ec9201f4a9b20',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://api.zeks.xyz': 'FeGgLPGEeREadOgAVcjAb13EADl',
 }
 
 // Sticker WM
